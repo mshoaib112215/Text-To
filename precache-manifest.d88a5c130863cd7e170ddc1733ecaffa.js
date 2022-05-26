71a526a2a28150b4a98c4b8c5b4fe0a5711c7d39
mshoaib112215 <75632120+mshoaib112215@users.noreply.github.com>
2022-05-26 22:52:19 +0500

    Updates

---
+++ precache-manifest.d88a5c130863cd7e170ddc1733ecaffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1316fbb3dfaecd271c8",
+    "url": "/Text-To/static/css/main.437364d7.chunk.css"
+  },
+  {
+    "revision": "a1316fbb3dfaecd271c8",
+    "url": "/Text-To/static/js/main.a1316fbb.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/Text-To/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/Text-To/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "fb3fe9caf1853169830c",
+    "url": "/Text-To/static/js/runtime~main.fb3fe9ca.js"
+  },
+  {
+    "revision": "e629d25779c6c58981c8b3179126c2cb",
+    "url": "/Text-To/index.html"
+  }
+];
