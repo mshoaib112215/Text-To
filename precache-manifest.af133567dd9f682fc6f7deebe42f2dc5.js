eaa3a2f913c8e8daddf9316d3ae61ff44f1ff7f1
mshoaib112215 <75632120+mshoaib112215@users.noreply.github.com>
2022-05-26 22:39:52 +0500

    My First Project of React.js :) Feeling Charm

---
+++ precache-manifest.af133567dd9f682fc6f7deebe42f2dc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7bdb66cb9cd8e1bac0bd",
+    "url": "/static/css/main.437364d7.chunk.css"
+  },
+  {
+    "revision": "7bdb66cb9cd8e1bac0bd",
+    "url": "/static/js/main.7bdb66cb.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "96cb2b49424a828879eb",
+    "url": "/static/js/runtime~main.96cb2b49.js"
+  },
+  {
+    "revision": "8ac9371d368da270a6ef37de8c8976e7",
+    "url": "/index.html"
+  }
+];
